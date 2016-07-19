125c9a01a3d595b021a4a16a1c698ab795e69d48
Jannik Kollmann <Jannik@Janniks-MacBook-Pro.local>
2016-07-19 22:00:29 +0100

    Kind of working resource handler

--- src/clj/lunch/handler.clj
+++ src/clj/lunch/handler.clj
@@ -1,6 +1,6 @@
 (ns lunch.handler
   (:require [ring.util.response :as res :refer [resource-response]]
-            [bidi.ring :refer [make-handler]]
+            [bidi.ring :refer [make-handler files]]
             [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn index-handler
@@ -11,9 +11,23 @@
   [{:keys [route-params]}]
   (res/response (str "You are viewing article: " (:id route-params))))
 
+(defn resource-handler
+  [request]
+  (resource-response (:uri request) {:root "public"}))
+
+(def routing-table
+  ["/" {"" :index
+        "api/" {["place/" :id] :api-place}
+        true :resources}])
+
+(def handler-map
+  {:index index-handler
+   :resources resource-handler
+   :api-place article-handler
+   })
+
 (def handler
-  (make-handler ["/" {"" index-handler
-                      "api/" {"place" article-handler}}]))
+  (make-handler routing-table handler-map))
 
 (def dev-handler (-> handler wrap-reload))
 
